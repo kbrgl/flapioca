76282303ce652cfcc133d2f73de521fd566426f3
Kabir Goel <kabirgoel.kg@gmail.com>
2022-07-18 19:25:18 -0700

    feat: rearchitect code and size to terminal window

--- internal/model.go
+++ internal/model.go
@@ -14,32 +14,40 @@ import (
 type TickMsg time.Time
 
 type Model struct {
-	Keys      KeyMap
+	// Keys holds key bindings.
+	Keys KeyMap
+	// Obstacles is a data structure containing obstacles.
 	Obstacles Obstacles
-	Cursor    Location
-	Score     int
-	Help      help.Model
-	Viewport  Location
-	Over      bool
-	Pressed   bool
+	// Cursor is the location of the cursor.
+	Cursor Location
+	// Score is the number of obstacles avoided.
+	Score int
+	// Help contains the Bubble Tea help model.
+	Help help.Model
+	// Viewport is the size of the game area.
+	Viewport Location
+	// Over is true when the player has lost.
+	Over bool
+	// Pressed is used to lock the cursor from moving until the next tick.
+	Pressed bool
+	// Layouted tracks whether the initial layout has been performed.
+	Layouted bool
 }
 
 func NewModel() Model {
-	viewport := Location{
-		X: 60,
-		Y: 9,
-	}
 	return Model{
 		Keys: KeyMap{
 			Up:   key.NewBinding(key.WithKeys("k", "up", " ", "w"), key.WithHelp("↑/k/w/space", "jump")),
 			Quit: key.NewBinding(key.WithKeys("q", "ctrl+c"), key.WithHelp("q/ctrl+c", "quit")),
 		},
-		Obstacles: NewObstacles(NewObstacle(2, &Location{viewport.X / 2, viewport.Y / 2})),
-		Cursor:    Location{4, viewport.Y / 2},
+		Obstacles: NewObstacles(),
+		Cursor:    Location{},
 		Score:     0,
 		Help:      help.New(),
 		Over:      false,
-		Viewport:  viewport,
+		Viewport:  Location{},
+		Pressed:   false,
+		Layouted:  false,
 	}
 }
 
@@ -72,50 +80,63 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		}
 	case tea.WindowSizeMsg:
 		// Terminal resized.
-		m.Help.Width = msg.Width
+		if !m.Layouted {
+			m.Layouted = true
+			m.Help.Width = msg.Width
+			m.Viewport.X = msg.Width - ViewportStyle.GetHorizontalBorderSize()
+			m.Viewport.Y = msg.Height - ViewportStyle.GetVerticalBorderSize() - 5
+			m.Cursor = Location{5, m.Viewport.Y / 2}
+		}
 	case TickMsg:
-		if !m.Pressed {
-			m.Cursor.Y++
-		}
-		m.Pressed = false
-		for _, obst := range m.Obstacles {
-			if (obst.Collides(m.Cursor)) || m.Cursor.Y >= m.Viewport.Y {
-				m.Over = true
-				return m, tea.Quit
-			}
-			if obst.Location.X == m.Cursor.X {
-				m.Score++
-			} else if obst.Location.X < 0 {
-				m.Obstacles.Remove()
-			}
-			obst.Location.X--
-		}
-		rightmost := m.Obstacles.Rightmost()
-		if rightmost == nil {
-			return m, tea.Quit
-		}
-		// Create a new obstacle some percent of the time.
-		gap := m.Viewport.X - rightmost.Location.X
-		if gap > 5 || (rand.Intn(100) > 90 && gap > 2) {
-			// Select a y that makes the obstacle possible to avoid.
-			x := m.Viewport.X
-			var y int
-			yDelta := rand.Intn(x - rightmost.Location.X)
-			if rand.Intn(100) > 50 {
-				y = rightmost.Location.Y + yDelta
-			} else {
-				y = rightmost.Location.Y - yDelta
-			}
-			if y < 0 {
-				y = 0
-			} else if y >= m.Viewport.Y {
-				y = m.Viewport.Y - 1
-			}
-			m.Obstacles.Add(NewObstacle(2, &Location{x, y}))
-		}
-		return m, m.tick()
+		return m.Frame()
 	}
 	return m, nil
+}
+
+func (m Model) Frame() (tea.Model, tea.Cmd) {
+	if !m.Pressed {
+		m.Cursor.Y++
+	}
+	m.Pressed = false
+	for _, obst := range m.Obstacles {
+		if (obst.Collides(m.Cursor)) || m.Cursor.Y >= m.Viewport.Y {
+			m.Over = true
+			return m, tea.Quit
+		}
+		if obst.Location.X == m.Cursor.X {
+			m.Score++
+		} else if obst.Location.X < 0 {
+			m.Obstacles.Remove()
+		}
+		obst.Location.X--
+	}
+	var rightmost *Location
+	if len(m.Obstacles) > 0 {
+		rightmost = m.Obstacles[len(m.Obstacles)-1].Location
+	} else {
+		rightmost = &Location{0, m.Viewport.Y / 2}
+	}
+
+	// Create a new obstacle in some situations.
+	gap := m.Viewport.X - rightmost.X
+	if gap > 5 || (rand.Intn(100) > 90 && gap > 2) {
+		// Select a y that makes the obstacle possible to avoid.
+		x := m.Viewport.X
+		var y int
+		yDelta := rand.Intn(x - rightmost.X)
+		if rand.Intn(100) > 50 {
+			y = rightmost.Y + yDelta
+		} else {
+			y = rightmost.Y - yDelta
+		}
+		if y < 0 {
+			y = 0
+		} else if y >= m.Viewport.Y {
+			y = m.Viewport.Y - 1
+		}
+		m.Obstacles.Add(NewObstacle(ApertureSize, &Location{x, y}))
+	}
+	return m, m.tick()
 }
 
 func (m Model) View() string {
