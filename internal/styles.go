5c7d1d8b2365cbbccaa48f74735e95f70f8532b2
Kabir Goel <kabirgoel.kg@gmail.com>
2022-07-18 18:53:32 -0700

    feat: update styling to make border transparent

--- internal/styles.go
+++ internal/styles.go
@@ -4,7 +4,7 @@ import "github.com/charmbracelet/lipgloss"
 
 var (
 	TitleStyle    = lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("10"))
-	ViewStyle     = lipgloss.NewStyle().MarginTop(1).MarginBottom(2)
-	ViewportStyle = lipgloss.NewStyle().Border(lipgloss.RoundedBorder())
+	ViewStyle     = lipgloss.NewStyle().MarginTop(1).MarginBottom(1)
+	ViewportStyle = lipgloss.NewStyle().Border(lipgloss.RoundedBorder()).BorderForeground(lipgloss.Color("236"))
 	GameOverStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("9"))
 )
