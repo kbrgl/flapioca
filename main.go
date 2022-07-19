fa16e38679be90f5f91de37b6de21cdbc5b6a239
Kabir Goel <kabirgoel.kg@gmail.com>
2022-07-18 18:53:51 -0700

    feat: seed PRNG

--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"math/rand"
+	"time"
+
 	"github.com/kbrgl/flapioca/internal"
 )
+
+func init() {
+	rand.Seed(time.Now().UTC().UnixNano())
+}
 
 func main() {
 	internal.Execute()
